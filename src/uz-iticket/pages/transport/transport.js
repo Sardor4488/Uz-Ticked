e9d20aa484d4d1a6df850c659c82ff178ff0cce2
Temirov Sardor <you@example.com>
2022-10-15 17:14:06 +0500

    transport section

--- src/uz-iticket/pages/transport/transport.js
+++ src/uz-iticket/pages/transport/transport.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import TransportWrapper from './transportWrapper'; 
-import { Box, Tab } from '@mui/material';
+import { Box, Stack, Tab } from '@mui/material';
 import TabContext from '@mui/lab/TabContext';
 import TabList from '@mui/lab/TabList';
 import TabPanel from '@mui/lab/TabPanel';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBus, faCalendar, faCalendarAlt, faPlane, faTrain, faTruckPlane } from '@fortawesome/free-solid-svg-icons';
+
 
 const Transport = () => {
-    const [value, setValue] = React.useState('2');
-
+    const [value, setValue] = React.useState('1');
+     
     const handleChange = (event, newValue) => {
         setValue(newValue);
     };
@@ -15,18 +18,22 @@ const Transport = () => {
     <TransportWrapper>
       <div className='transport-section'>
         <div class="container">
-          <div className='cont-transports-search'>
+          <div className='cont-transports-search'> 
               <Box sx={{ width: '100%', typography: 'body1' }}>
                   <TabContext value={value}>
                       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
                           <TabList onChange={handleChange} aria-label="lab API tabs example">
-                              <Tab className='fw-bold' label="Barcha reyslar" value="1" />
-                              <Tab className='fw-bold'  label="Samalyotlar" value="2" />
-                              <Tab className='fw-bold'  label="Poyezdlar" value="3" />
-                              <Tab className='fw-bold'  label="Avtobuslar" value="4" />
+                              <Tab className='' label={< FontAwesomeIcon icon={faTruckPlane} />} value="1" />
+                              <Tab className=''  label={< FontAwesomeIcon icon={faPlane}/>} value="2" />
+                              <Tab className=''  label={< FontAwesomeIcon icon={faTrain}/>} value="3" />
+                              <Tab className=''  label={< FontAwesomeIcon icon={faBus}/>} value="4" />
                           </TabList>
                       </Box>
-                      <TabPanel value="1">Barcha reyslar</TabPanel>
+                      <TabPanel value="1">
+                        <div className='date-transports'>
+                        < FontAwesomeIcon className='text-dark' icon={faCalendarAlt}/>
+                        </div>
+                      </TabPanel>
                       <TabPanel value="2">Samalyotlar</TabPanel>
                       <TabPanel value="3">Poyezdlar</TabPanel>
                       <TabPanel value="4">Avtobuslar</TabPanel>
--- src/uz-iticket/pages/transport/transportWrapper.js
+++ src/uz-iticket/pages/transport/transportWrapper.js
@@ -11,10 +11,17 @@ const TransportWrapper = styled.div`
     background-image: url(https://cdn1.tu-tu.ru/images2/bemp/page/main/etrains_3.jpg);
     
 } 
+.date-transports{
+    width: 70px !important;
+    background-color: #f2f2f2;
+    border-radius: 7px;
+    padding: 10px;
+}
 .css-1h9z7r5-MuiButtonBase-root-MuiTab-root{
     padding: 0 0 0 5px;
-    font-size: 10px;
-}
+    font-size: 16px; 
+    min-width: 70px !important;
+} 
 .cont-transports-search{
     border-radius: 8px;
     background-color: #fff;
